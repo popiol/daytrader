cc7f7ac7804490c58b96a58efe0f38927246a9f9
popiol <opiola.piotr@gmail.com>
2020-02-08 19:26:34 +0100

    Auto commit 2020-02-08 19:26:34

--- terraform/schedule.tf
+++ terraform/schedule.tf
@@ -13,12 +13,5 @@ resource "aws_cloudwatch_event_rule" "get_quotes" {
 resource "aws_cloudwatch_event_target" "get_quotes" {
 	rule = aws_cloudwatch_event_rule.get_quotes.name
 	arn = aws_lambda_function.l_get_quotes.arn
-
-  tags = {
-    App = var.app
-    AppVer = var.app_ver
-    AppStage = var.app_stage
-    TerraformID = "aws_cloudwatch_event_target.get_quotes"
-  }
 }
 
