a9787d7f4710be3471b8bee33e25f141c53d31c7
popiol <opiola.piotr@gmail.com>
2020-02-08 21:54:44 +0100

    Auto commit 2020-02-08 21:54:44

--- terraform/schedule.tf
+++ terraform/schedule.tf
@@ -11,6 +11,7 @@ resource "aws_cloudwatch_event_rule" "get_quotes" {
 }
 
 resource "aws_cloudwatch_event_target" "get_quotes" {
+	target_id = "${var.app_id}_get_quotes"
 	rule = aws_cloudwatch_event_rule.get_quotes.name
 	arn = aws_lambda_function.l_get_quotes.arn
 }
