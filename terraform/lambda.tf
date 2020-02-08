3c9825b38c9afff65eff292835dc704f332a969e
popiol <opiola.piotr@gmail.com>
2020-02-08 01:19:18 +0100

    Auto commit 2020-02-08 01:19:18

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,23 +1,16 @@
-resource "aws_lambda_function" "l_hello_world" {
-  filename      = "lambda/hello_world.zip"
-  function_name = "${var.app_id}_hello_world"
+resource "aws_lambda_function" "l_get_quotes" {
+  filename      = "lambda/get_quotes.zip"
+  function_name = "${var.app_id}_get_quotes"
   role          = aws_iam_role.lambdarole.arn
   handler       = "main.lambda_handler"
 
-  source_code_hash = filebase64sha256("lambda/hello_world.zip")
+  source_code_hash = filebase64sha256("lambda/get_quotes.zip")
 
   runtime = "python3.8"
 
   vpc_config {
     subnet_ids         = [aws_subnet.subnet1.id]
     security_group_ids = [aws_vpc.vpc1.default_security_group_id]
-  }
-
-  tags = {
-    App = var.app
-    AppVer = var.app_ver
-    AppStage = var.app_stage
-    TerraformID = "aws_lambda_function.l_hello_world"
   }
 }
 
