75962d5029df4a97506b83f76ee17d1bc5da1191
popiol <opiola.piotr@gmail.com>
2020-05-10 21:08:46 +0200

    add explicit packags to setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name = "daytrader",
     version = ">=3.8",
-    packages = find_packages(),
+    packages = find_packages() + [
+        'pandas',
+        'numpy',
+        'boto3',
+        'pytest',
+    ],
 )
