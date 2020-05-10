e50217fc4a4ef44c8bc026d57f75540ef1b13f19
popiol <opiola.piotr@gmail.com>
2020-05-10 23:21:52 +0200

    test_remote

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "daytrader",
+    version = "1.0.0",
     python_requires = ">=3.8",
     packages = find_packages(),
     install_requires = [
--- test/test_get_quotes.py
+++ test/test_get_quotes.py
@@ -4,6 +4,7 @@ import importlib
 import os
 import sys
 import pytest
+import json
 
 class TestGetQuotes():
 
@@ -22,11 +23,7 @@ class TestGetQuotes():
         vars['bucket_name'] = "{}.{}-quotes".format(vars['aws_user'], vars['id'].replace('_','-'))
         return vars
 
-    def test_local(self, vars):
-        bucket_name = vars['bucket_name']
-        sys.path.insert(0, os.getcwd())
-        get_quotes = importlib.import_module("lambda.get_quotes.main")
-        res = get_quotes.lambda_handler({"bucket_name": bucket_name}, {})
+    def check_result(self, bucket_name, res):
         assert res['statusCode'] == 200
         assert res['body']['bucket_name'] == bucket_name
         assert len(res['body']['files']) > 0
@@ -42,3 +39,22 @@ class TestGetQuotes():
             assert re.search("<table.+<th.+High.*</th>", html, re.DOTALL)
             assert re.search("<table.+<th.+Time.*</th>", html, re.DOTALL)
             assert re.search("<table.+<th.+Date.*</th>", html, re.DOTALL)
+
+    def test_local(self, vars):
+        bucket_name = vars['bucket_name']
+        sys.path.insert(0, os.getcwd())
+        get_quotes = importlib.import_module("lambda.get_quotes.main")
+        res = get_quotes.lambda_handler({"bucket_name": bucket_name}, {})
+        self.check_result(bucket_name, res)
+
+    def test_remote(self, vars):
+        bucket_name = vars['bucket_name']
+        fun = boto3.client('lambda')
+        res = fun.invoke(
+            FunctionName = vars['id'] + '_get_quotes',
+            InvocationType = 'RequestResponse',
+            LogType = 'None',
+            Payload = bytearray(json.dumps(vars)),
+        )
+        self.check_result(bucket_name, res)
+        
