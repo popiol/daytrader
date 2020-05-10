084240256f6326df358998d8df2a59e814890084
popiol <opiola.piotr@gmail.com>
2020-05-10 20:02:35 +0200

    fix pytest

--- test/test_get_quotes.py
+++ test/test_get_quotes.py
@@ -3,11 +3,13 @@ import re
 import importlib
 import os
 import sys
+import pytest
 
 class TestGetQuotes():
-    
-    def __init__(self):
-        self.vars = {}
+
+    @pytest.fixture(scope='class')
+    def vars(self):
+        vars = {}
         with open('config.tfvars','r') as f:
             for line in f:
                 if '=' not in line:
@@ -16,22 +18,23 @@ class TestGetQuotes():
                 key = key.strip()
                 val = val.strip()
                 if val[0] == '"' and val[-1] == '"':
-                    self.vars[key] = val[1:-1]
-        self.bucket_name = "{}.{}-quotes".format(self.vars['aws_user'], self.vars['id'].replace('_','-'))
+                    vars[key] = val[1:-1]
+        vars['bucket_name'] = "{}.{}-quotes".format(vars['aws_user'], vars['id'].replace('_','-'))
+        return vars
 
-
-    def test_local(self):
+    def test_local(self, vars):
+        bucket_name = vars['bucket_name']
         sys.path.insert(0, os.getcwd())
         get_quotes = importlib.import_module("lambda.get_quotes.main")
-        res = get_quotes.lambda_handler({"bucket_name": self.bucket_name}, {})
+        res = get_quotes.lambda_handler({"bucket_name": bucket_name}, {})
         assert res['statusCode'] == 200
-        assert res['body']['bucket_name'] == self.bucket_name
+        assert res['body']['bucket_name'] == bucket_name
         assert len(res['body']['files']) > 0
         files = res['body']['files']
         s3 = boto3.resource('s3')
         for key in files:
             print("file =", key)
-            obj = s3.Object(self.bucket_name, key)
+            obj = s3.Object(bucket_name, key)
             html = obj.get()['Body'].read().decode('utf-8')
             assert re.match("<table.+<th.+Name.*</th>", html, re.DOTALL)
             assert re.match("<table.+<th.+Latest Price.*</th>", html, re.DOTALL)
