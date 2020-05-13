571d0a325eca2188d5edf401641823f540e7dbc7
popiol <opiola.piotr@gmail.com>
2020-05-13 03:53:07 +0200

    test clean quotes fix

--- test/test_clean_quotes.py
+++ test/test_clean_quotes.py
@@ -60,8 +60,9 @@ class TestCleanQuotes():
         bucket = s3.Bucket(bucket_name)
         files = []
         for obj in bucket.objects.all():
-            if obj.last_modified.strftime('%Y%m%d%H%M%S') >= vars['timestamp'] 
-            and (obj.key.startswith('csv_clean/') or obj.key.startswith('csv_rejected/')):
+            if obj.last_modified.strftime('%Y%m%d%H%M%S') >= vars['timestamp'] and (
+                obj.key.startswith('csv_clean/') or obj.key.startswith('csv_rejected/')
+            ):
                 files.append(obj.key)
         vars['keys'] = files
         return vars
