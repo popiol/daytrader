3a5a049a6f7d5484fc3d366f34f7d335eccc5ff2
popiol <opiola.piotr@gmail.com>
2020-05-16 13:56:51 +0200

    alerts topic "_" -> "-"

--- sns/sns.tf
+++ sns/sns.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudformation_stack" "main" {
 	name = replace("${var.inp.app.id}-${var.topic}","_","-")
 	template_body = templatefile("${path.module}/sns.json", {
-		topic = "${var.inp.app.id}_${var.topic}"
+		topic = replace("${var.inp.app.id}-${var.topic}","_","-")
 		display_name  = "Daytrader Alerts"
 		subscribe = jsonencode(var.subscribe)
 	})
