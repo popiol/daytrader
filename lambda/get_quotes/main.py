17e9cff0282e4ffe6b9ba3946a0c3aabc0a10594
popiol <opiola.piotr@gmail.com>
2020-05-16 20:54:54 +0200

    get_quotes fix

--- lambda/get_quotes/main.py
+++ lambda/get_quotes/main.py
@@ -6,13 +6,13 @@ import datetime
 import dateutil.tz
 
 def lambda_handler(event, context):
-    s3 = boto3.client('s3')
+    s3 = boto3.resource('s3')
     bucket_name = event['bucket_name']
+    bucket = s3.Bucket(bucket_name)
     
     #alert if quotes missing
     dt = datetime.datetime.now(dateutil.tz.gettz('US/Eastern'))
     if dt.hour > 9:
-        bucket = s3.Bucket(bucket_name)
         hour = datetime.timedelta(hours=1)
         dt2 = (datetime.datetime.now()-hour).strftime('%Y%m%d%H')
         objs = bucket.filter(Prefix="/html/businessinsider_{}".format(dt2))
@@ -34,7 +34,7 @@ def lambda_handler(event, context):
         url = url_templ.format(n)
         data = urlopen(url)
         file_name = file_name_templ.format(dt, n)
-        s3.upload_fileobj(data, bucket_name, file_name)
+        bucket.upload_fileobj(data, file_name)
         res['files'].append(file_name)
         
     return {
