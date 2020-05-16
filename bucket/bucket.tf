bc8b17dfcd073193030d7b6ead53c186603abbc4
popiol <opiola.piotr@gmail.com>
2020-05-16 22:20:26 +0200

    listbucket fix

--- bucket/bucket.tf
+++ bucket/bucket.tf
@@ -30,11 +30,19 @@ data "aws_iam_policy_document" "access" {
 		actions = [
 			"s3:GetObject",
 			"s3:PutObject",
-			"s3:DeleteObject",
-			"s3:ListBucket"
+			"s3:DeleteObject"
 		]
 		resources = [
 			"${aws_s3_bucket.main.arn}/*"
+		]
+	}
+
+	statement {
+		actions = [
+			"s3:ListBucket"
+		]
+		resources = [
+			"${aws_s3_bucket.main.arn}"
 		]
 	}
 }
