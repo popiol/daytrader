925051a8f5ac0b1b747560c6773e2d674690a7bd
popiol <opiola.piotr@gmail.com>
2020-05-20 15:03:28 +0200

    default args without jsonencode

--- glue/pythonjob/pythonjob.tf
+++ glue/pythonjob/pythonjob.tf
@@ -11,7 +11,8 @@ resource "aws_glue_job" "main" {
 	}
 
 	default_arguments = {
-		for key, val in var.inp : "--${key}" => jsonencode(val)
+#		for key, val in var.inp : "--${key}" => regex("^\"*(.*[^\"])\"*$", jsonencode(val))[0]
+		for key, val in var.inp : "--${key}" => val
 	}
 }
 
