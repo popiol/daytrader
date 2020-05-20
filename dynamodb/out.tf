bb437fce667db00600dcf3830319ef0bbfe3a07b
popiol <opiola.piotr@gmail.com>
2020-05-20 14:05:32 +0200

    typo fix

--- dynamodb/out.tf
+++ dynamodb/out.tf
@@ -1,13 +1,13 @@
 output "access_policy" {
   value = {
     event_process_log = module.event_process_log.access_policy
-    evant_table = module.evant_table.access_policy
+    evant_table = module.event_table.access_policy
   }
 }
 
 output "table_name" {
   value = {
     event_process_log = module.event_process_log.table_name
-    evant_table = module.evant_table.table_name
+    evant_table = module.event_table.table_name
   }
 }
