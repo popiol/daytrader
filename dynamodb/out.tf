cff9ae875081d0537ef1bfe6054a8167328399d6
popiol <opiola.piotr@gmail.com>
2020-05-20 13:37:46 +0200

    debug

--- dynamodb/out.tf
+++ dynamodb/out.tf
@@ -1,7 +1,7 @@
 output "access_policy" {
-  value = {for x in module[*]: x => module[x].access_policy}
+  value = {for x in module.table: x => module.x.access_policy}
 }
 
 output "table_name" {
-  value = {for x in module[*]: x => module[x].table_name}
+  value = {for x in module.table: x => module.x.table_name}
 }
