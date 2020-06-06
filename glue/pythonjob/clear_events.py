733d6a4b72307bcaeb5a0e8680fa41fbc6c329ec
popiol <opiola.piotr@gmail.com>
2020-06-06 11:39:18 +0200

    truncate table

--- glue/pythonjob/clear_events.py
+++ glue/pythonjob/clear_events.py
@@ -39,9 +39,8 @@ for batch_n in range(math.ceil(len(files)/1000)):
 def truncate_table(db, table_name):
     table = db.describe_table(TableName=table_name)['Table']
     table['BillingMode'] = table['BillingModeSummary']['BillingMode']
-    table['ProvisionedThroughput'] = {x: table['ProvisionedThroughput'][x] for x in ['ReadCapacityUnits','WriteCapacityUnits']}
     db.delete_table(TableName=table_name)
-    arg_keys = ['AttributeDefinitions', 'TableName', 'KeySchema', 'LocalSecondaryIndexes', 'GlobalSecondaryIndexes', 'BillingMode', 'ProvisionedThroughput', 'StreamSpecification', 'SSESpecification', 'Tags']
+    arg_keys = ['AttributeDefinitions', 'TableName', 'KeySchema', 'LocalSecondaryIndexes', 'GlobalSecondaryIndexes', 'BillingMode', 'StreamSpecification', 'SSESpecification', 'Tags']
     args = {x: table[x] for x in arg_keys if x in table}
     db.create_table(**args)
 
