0fdf933e742e61a0abb2a0667dcc1542bf7f7eee
popiol <opiola.piotr@gmail.com>
2020-07-03 23:42:12 +0200

    vcpus fix

--- batch_jobs/batchjob/batchjob.tf
+++ batch_jobs/batchjob/batchjob.tf
@@ -5,7 +5,7 @@ resource "aws_batch_job_definition" "main" {
         command = ["python", "${var.job_name}.py"]
         image = "${var.inp.aws_user_id}.dkr.ecr.${var.inp.aws_region}.amazonaws.com/${var.image_name}"
         memory = 1500
-        vcpus = 1.5
+        vcpus = 2
         environment = [
             {
                 name = "BUCKET_NAME"
