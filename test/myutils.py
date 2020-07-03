41ebbf9b659b3f0c32b22c87092499565f51b387
popiol <opiola.piotr@gmail.com>
2020-07-03 12:09:51 +0200

    terraform init

--- test/myutils.py
+++ test/myutils.py
@@ -37,6 +37,7 @@ def get_vars():
     vars['alert_topic'] = sns_arn
 
     #get terraform outputs
+    os.system('terraform init')
     os.system('terraform output > out.txt')
     with open('out.txt','r') as f:
         for line in f:
