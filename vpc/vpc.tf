91817a3f8bb54bc2b392f1ddaf975b9cf75493e1
popiol <opiola.piotr@gmail.com>
2020-07-05 17:03:58 +0200

    default routing table

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -40,9 +40,9 @@ resource "aws_internet_gateway" "main" {
     tags = var.inp.app
 }
 
-resource "aws_route_table" "main" {
-	vpc_id = aws_vpc.main.id
-	tags = var.inp.app
+resource "aws_default_route_table" "main" {
+    default_route_table_id = aws_vpc.main.default_route_table_id
+    tags = var.inp.app
 
     route {
 		cidr_block = "0.0.0.0/0"
@@ -50,7 +50,3 @@ resource "aws_route_table" "main" {
 	}
 }
 
-resource "aws_main_route_table_association" "main" {
-    vpc_id = aws_vpc.main.id
-    route_table_id = aws_route_table.main.id
-}
