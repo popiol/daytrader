6e493139b78c712cd322607931c3184422759b6d
popiol <opiola.piotr@gmail.com>
2020-07-12 01:39:03 +0200

    print quotes

--- restapi/restapi.tf
+++ restapi/restapi.tf
@@ -43,14 +43,3 @@ resource "aws_lambda_permission" "main" {
 	principal = "apigateway.amazonaws.com"
 	source_arn = "arn:aws:execute-api:${var.inp.aws_region}:${var.inp.aws_user_id}:${aws_api_gateway_rest_api.main.id}/*/${aws_api_gateway_method.main.http_method}${aws_api_gateway_resource.main.path}"
 }
-
-#resource "aws_api_gateway_method_response" "main" {
-#    rest_api_id = aws_api_gateway_rest_api.main.id
-#    resource_id = aws_api_gateway_resource.main.id
-#    http_method = aws_api_gateway_method.main.http_method
-#    status_code = "200"
-#    response_parameters = {
-#        "method.response.header.Access-Control-Allow-Origin" = true
-#    }
-#    depends_on = [aws_api_gateway_method.main]
-#}
--- www/sample_plots.js
+++ www/sample_plots.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
     $.ajax({
         url: "${get_sample_quotes_url}?job_name=train_init"
     }).done(function(data){
-        $("#plots").text(data)
+        for (comp_code in data) {
+            $("#plots").append("<h2>"+comp_code+"</h2>")
+            $("#plots").append("<p>"+JSON.stringify(data[comp_code])+"</p>")
+        }
     })
 })
