fa83cac2d939a8130326dfab99ad01eec37a33d2
popiol <opiola.piotr@gmail.com>
2020-07-11 13:20:16 +0200

    get - post

--- lambda/lambda.tf
+++ lambda/lambda.tf
@@ -72,7 +72,7 @@ resource "aws_api_gateway_integration" "main" {
 	rest_api_id = aws_api_gateway_rest_api.main["0"].id
 	resource_id = aws_api_gateway_resource.main["0"].id
 	http_method = aws_api_gateway_method.main["0"].http_method
-	integration_http_method = "GET"
+	integration_http_method = "POST"
 	type = "AWS_PROXY"
 	uri = aws_lambda_function.main.invoke_arn
 }
