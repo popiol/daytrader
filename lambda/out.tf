61d398939b2c94ff63c76d71ef13dbcf9d917aaf
popiol <opiola.piotr@gmail.com>
2020-07-11 12:10:10 +0200

    invoke url

--- lambda/out.tf
+++ lambda/out.tf
@@ -3,5 +3,5 @@ output "arn" {
 }
 
 output "invoke_url" {
-    value = var.rest_api ? "aws_api_gateway_deployment.main[0].invoke_url${aws_api_gateway_resource.main["0"].path}" : ""
+    value = var.rest_api ? "${aws_api_gateway_deployment.main[0].invoke_url}${aws_api_gateway_resource.main["0"].path}" : ""
 }
