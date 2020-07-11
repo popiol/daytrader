0249a1a17cdda9a9337e7d2678cfe252fd72d2e0
popiol <opiola.piotr@gmail.com>
2020-07-12 00:03:56 +0200

    main[0 -> main

--- restapi/restapi.tf
+++ restapi/restapi.tf
@@ -10,8 +10,8 @@ resource "aws_api_gateway_resource" "main" {
 }
 
 resource "aws_api_gateway_method" "main" {
-	rest_api_id = aws_api_gateway_rest_api.main[0].id
-	resource_id = aws_api_gateway_resource.main[0].id
+	rest_api_id = aws_api_gateway_rest_api.main.id
+	resource_id = aws_api_gateway_resource.main.id
 	http_method = "GET"
 	authorization = "NONE"
 	request_parameters = {for x in var.query_params : x => true}
