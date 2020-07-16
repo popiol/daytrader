c525b8e398cf7753501d8dd3e816dd2e0d61880b
popiol <opiola.piotr@gmail.com>
2020-07-16 19:50:41 +0200

    remove debug

--- pythonjob/glue_utils.py
+++ pythonjob/glue_utils.py
@@ -397,7 +397,6 @@ class HistSimulator():
 
     def next(self):
         self.quote_dt = get_start_dt(self.event_table, self.quote_dt)
-        print("quote_dt", self.quote_dt)
         if self.quote_dt is None:
             return None
         res = self.event_table.scan(
