be5c56b26d48040d01580dc0bbe2dec9aa3b019f
popiol <opiola.piotr@gmail.com>
2020-07-18 21:57:34 +0200

    remove desired cpus

--- batch.tf
+++ batch.tf
@@ -20,7 +20,6 @@ resource "aws_batch_compute_environment" "main" {
 		instance_role = aws_iam_instance_profile.batch.arn
 		max_vcpus = 2
 		min_vcpus = 0
-		desired_vcpus = 0
 		security_group_ids = module.vpc.security_groups
 		subnets = module.vpc.subnets
 		type = "EC2"
