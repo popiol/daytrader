a14f894d36a956916a0472489f838ca1083dbe5e
popiol <opiola.piotr@gmail.com>
2020-07-20 10:59:02 +0200

    fix triggers

--- workflow.tf
+++ workflow.tf
@@ -111,17 +111,17 @@ resource "aws_glue_trigger" "start_events" {
 resource "aws_glue_trigger" "train_model" {
 	name = "${var.inp.app.id}_train_model"
 	type = "SCHEDULED"
-	schedule = "cron(13-22 5 ? * * *)"
+	schedule = "cron(13-22 3 ? * * *)"
 
 	actions {
-		job_name = module.glue_test_model.job_name
+		job_name = module.glue_train_model.job_name
 	}
 }
 
 resource "aws_glue_trigger" "test_model" {
 	name = "${var.inp.app.id}_test_model"
 	type = "SCHEDULED"
-	schedule = "cron(13 9 ? * * *)"
+	schedule = "cron(13 7 ? * * *)"
 
 	actions {
 		job_name = module.glue_test_model.job_name
