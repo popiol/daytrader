f4c582b41477fa4b312340f4da9fe685f0916a63
popiol <opiola.piotr@gmail.com>
2020-07-22 03:45:41 +0200

    move along gradient

--- docker/ml/app/ml_utils.py
+++ docker/ml/app/ml_utils.py
@@ -221,13 +221,17 @@ class Agent():
 
     def get_train_outputs(self, events, inputs):
         outputs = self.get_test_outputs(events, inputs)
+        self.grad = []
         for comp_code in outputs:
-            outputs[comp_code] = [min(1, max(-1, x + random.uniform(-.1, .1))) for x in outputs[comp_code]]
+            grad = [min(1, max(-1, x + random.uniform(-.1, .1))) for x in outputs[comp_code]]
+            outputs[comp_code] = grad
+            self.grad.append(grad)
         return outputs
 
     def train(self, events):
         inputs, outputs = self.next(events, self.get_train_outputs)
         self.fit(inputs, outputs)
+        return inputs, outputs, self.grad
         
 def compare_agents(agent1, agent2, hist=False, quick=False):
     scores1 = []
--- docker/ml/app/train_model.py
+++ docker/ml/app/train_model.py
@@ -15,16 +15,39 @@ best_score = None
 for _ in range(10):
     dev = ml_utils.Agent('current', ml_utils.bucket)
     events = simulator.next()
-    dev.train(events)
+    inputs, outputs, grad = dev.train(events)
     dev.reset()
     for events in hist:
         dev.test(events)
     if best_score is None or dev.score > best_score:
         best_dev = dev
         best_score = dev.score
+        best_inp = inputs
+        best_out = outputs
+        best_grad = grad
     print("Capital:", dev.get_capital())
+    print("Score:", dev.score)
 
-dev = best_dev
+print("---- Best score", best_score, "----")
+best_dev.save_as('dev')
+for _ in range(10):
+    outputs = []
+    for outputs1, grad in zip(best_out, best_grad):
+        outputs.append([x + y for x, y in zip(outputs1, grad)])
+    best_dev.fit(best_inp, outputs)
+    best_dev.reset()
+    for events in hist:
+        best_dev.test(events)
+    print("Capital:", best_dev.get_capital())
+    print("Score:", best_dev.score)
+    if best_dev.score > best_score:
+        best_score = best_dev.score
+        best_dev.save()
+        best_out = outputs
+    else:
+        break
+
+dev = ml_utils.Agent('dev', ml_utils.bucket)
 current = ml_utils.Agent('current', ml_utils.bucket)
 score1, score2 = ml_utils.compare_agents(dev, current, quick=True)
 print("Dev score:", score1, ", Current score:", score2)
