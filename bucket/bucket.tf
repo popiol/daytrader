07c8b3f5cc33597efb86a327fcaad829dd561e52
popiol <opiola.piotr@gmail.com>
2020-07-23 15:08:46 +0200

    revert

--- bucket/bucket.tf
+++ bucket/bucket.tf
@@ -1,32 +1,8 @@
-locals {
-	rules = {
-		rule1 = {
-			prefix = "data/"
-			expiration = 90
-		}
-		rule2 = {
-			prefix = "logs/"
-			expiration = 60
-		}
-	}
-}
-
 resource "aws_s3_bucket" "main" {
 	bucket = "${var.inp.aws_user}.${replace(var.inp.app.id,"_","-")}-${var.bucket_name}"
 	acl = "private"
 	tags = var.inp.app
 	force_destroy = var.inp.temporary
-
-	dynamic "lifecycle_rule" {
-		for_each = local.rules
-		content {
-			enabled = true
-			prefix = lifecycle_rule.value["prefix"]
-			expiration {
-				days = lifecycle_rule.value["expiration"]
-			}
-		}
-	}
 
 	dynamic "lifecycle_rule" {
 		for_each = toset(var.archived_paths)
