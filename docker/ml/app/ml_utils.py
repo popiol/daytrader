9569d6476f54711911ffdfd033945b0da4e063fc
popiol <opiola.piotr@gmail.com>
2020-07-23 23:16:05 +0200

    first_event fix

--- docker/ml/app/ml_utils.py
+++ docker/ml/app/ml_utils.py
@@ -179,11 +179,11 @@ class Agent():
             del self.event_hist[0]
         for event in events:
             comp_code = event.event['comp_code']
-            first_event = self.event_hist[0]
+            first_event = self.event_hist[0][comp_code]
             min_gain1 = None
             max_gain = None
             for prev_event in self.event_hist[1:]:
-                gain = prev_event[comp_code].get_price() / first_event[comp_code].get_price() - 1
+                gain = prev_event[comp_code].get_price() / first_event.get_price() - 1
                 if min_gain1 is None:
                     min_gain1 = gain
                 else:
