9d8ab803993206f0ddfdbffaddeb1bb0870e0d17
popiol <opiola.piotr@gmail.com>
2020-07-24 02:53:02 +0200

    quick train init

--- docker/ml/app/train_init.py
+++ docker/ml/app/train_init.py
@@ -6,8 +6,8 @@ import warnings
 warnings.filterwarnings("ignore")
 
 simulator = glue_utils.Simulator(ml_utils.bucket)
-initial = ml_utils.Agent('initial', ml_utils.bucket)
-for _ in range(1000):
+initial = ml_utils.Agent('current', ml_utils.bucket)
+for _ in range(100):
     events = simulator.next()
     initial.train_init(events)
 initial.save()
@@ -16,7 +16,7 @@ print("Capital:", initial.get_capital())
 simulator.print_sample_quotes()
 
 current = ml_utils.Agent('current', ml_utils.bucket)
-score1, score2 = ml_utils.compare_agents(initial, current)
+score1, score2 = ml_utils.compare_agents(initial, current, quick=True)
 print("Initial score:", score1, ", Current score:", score2)
 if score1 > score2:
     initial.save_as('current')
