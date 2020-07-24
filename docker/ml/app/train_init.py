f07cd05ab61123da90a495a0486b8663af563c37
popiol <opiola.piotr@gmail.com>
2020-07-24 03:05:29 +0200

    save as initial

--- docker/ml/app/train_init.py
+++ docker/ml/app/train_init.py
@@ -10,7 +10,7 @@ initial = ml_utils.Agent('current', ml_utils.bucket)
 for _ in range(100):
     events = simulator.next()
     initial.train_init(events)
-initial.save()
+initial.save_as('initial')
 print("Capital:", initial.get_capital())
 
 simulator.print_sample_quotes()
