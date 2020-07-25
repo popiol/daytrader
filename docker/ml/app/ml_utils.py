0986e057ee6523ea304c45809fbe11e7b2646832
popiol <opiola.piotr@gmail.com>
2020-07-25 03:17:19 +0200

    buy/sell price

--- docker/ml/app/ml_utils.py
+++ docker/ml/app/ml_utils.py
@@ -206,10 +206,10 @@ class Agent():
             if max_gain is not None:
                 inputs1 = self.get_inputs(first_event)
                 inputs.append(inputs1)
-                buy_action = max_gain - min_gain2
+                buy_action = max_gain
                 buy_action = 100 * buy_action / (1 + 100 * abs(buy_action))
-                buy_price = min_gain2 - .002
-                sell_price = max_gain + .002
+                buy_price = min(0, min_gain2 / 2)
+                sell_price = max(0, max_gain / 2)
                 output1 = [buy_action, buy_price, sell_price]
                 output1 = [(x+1)/2 for x in output1]
                 outputs.append(output1)
