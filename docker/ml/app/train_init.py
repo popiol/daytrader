efb9eb30107f5464b0616a23551eaaba4972cd4c
popiol <opiola.piotr@gmail.com>
2020-07-27 00:01:03 +0200

    logging

--- docker/ml/app/train_init.py
+++ docker/ml/app/train_init.py
@@ -29,7 +29,6 @@ initial = ml_utils.Agent('initial', ml_utils.bucket)
 current = ml_utils.Agent('current', ml_utils.bucket)
 score1, score2 = ml_utils.compare_agents(initial, current)
 print("Initial score:", score1, ", Current score:", score2)
-print("Capital:", initial.get_capital(), current.get_capital())
 if score1 > score2:
     initial.save_as('current')
 print()
