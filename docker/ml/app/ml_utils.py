bd87e6248428799fe3ed5c2a18c9be4174436240
popiol <opiola.piotr@gmail.com>
2020-07-30 21:58:54 +0200

    buy action

--- docker/ml/app/ml_utils.py
+++ docker/ml/app/ml_utils.py
@@ -211,7 +211,7 @@ class Agent():
                 inputs1 = self.get_inputs(first_event)
                 inputs.append(inputs1)
                 buy_action = max_gain + min_gain1
-                buy_action = 40000000 * buy_action / (1 + 80000000 * abs(buy_action))
+                buy_action = 400000000 * buy_action / (1 + 800000000 * abs(buy_action))
                 buy_price = max(-1, min(0, min_gain2 * 85))
                 sell_price = min(1, max(0, max_gain * 80))
                 output1 = [buy_action, buy_price, sell_price]
