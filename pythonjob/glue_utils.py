6479d72c75fa37a1762b22b868bb2d0d7c1c5de2
popiol <opiola.piotr@gmail.com>
2020-08-05 20:36:21 +0200

    price ch 1.7

--- pythonjob/glue_utils.py
+++ pythonjob/glue_utils.py
@@ -162,7 +162,7 @@ class Discretizer():
             start = max(start-(end-start)/2, -.9)
             end = min(end+(end-start)/2, 1.9)
             val = random.uniform(start, end)
-            val = val + .0001 * (offset-1)
+            val = (val + .0001 * (offset-1)) * 1.7
             outputs.append(val)
         return outputs
 
