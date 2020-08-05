ba46cf3308478aabf65dd6d2bad8eb02b8dffa8c
popiol <opiola.piotr@gmail.com>
2020-08-05 21:52:23 +0200

    remove quick

--- docker/ml/app/test_model.py
+++ docker/ml/app/test_model.py
@@ -7,16 +7,16 @@ warnings.filterwarnings("ignore")
 
 current = ml_utils.Agent('current', ml_utils.bucket)
 prod = ml_utils.Agent('prod', ml_utils.bucket)
-score1, score2 = ml_utils.compare_agents(current, prod, quick=True)
+score1, score2 = ml_utils.compare_agents(current, prod)
 print("Current score:", score1, ", Prod score:", score2)
 print("Price up/down:", np.average(current.price_up), "/", np.average(current.price_down))
 if score1 > score2:
-    score1, score2 = ml_utils.compare_agents(current, prod, hist=True, quick=True)
+    score1, score2 = ml_utils.compare_agents(current, prod, hist=True)
     print("Current score:", score1, ", Prod score:", score2)
     print("Price up/down:", np.average(current.price_up), "/", np.average(current.price_down))
     if score1 > score2:
         current.save_as('prod')
 else:
-    score1, score2 = ml_utils.compare_agents(current, prod, hist=True, quick=True)
+    score1, score2 = ml_utils.compare_agents(current, prod, hist=True)
     print("Current score:", score1, ", Prod score:", score2)
     print("Price up/down:", np.average(current.price_up), "/", np.average(current.price_down))
