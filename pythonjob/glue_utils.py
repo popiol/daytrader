a40ee100e9a61ab3dd3e7898d1ef6b5bd8758919
popiol <opiola.piotr@gmail.com>
2020-08-06 17:39:24 +0200

    offset, scale

--- pythonjob/glue_utils.py
+++ pythonjob/glue_utils.py
@@ -162,9 +162,9 @@ class Discretizer():
             start = max(start-(end-start)/2, -.9)
             end = min(end+(end-start)/2, 1.9)
             val = random.uniform(start, end)
-            val = val + .0001 * (offset-2)
+            val = val + .0001 * (offset-1)
             #val *= 1.5 if val > 0 else 1.8
-            val = math.pow(val+1, 2)-1 if val > 0 else math.pow(val+1, 3)-1
+            val = math.pow(val+1, 1.3)-1 if val > 0 else math.pow(val+1, 1.6)-1
             outputs.append(val)
         return outputs
 
