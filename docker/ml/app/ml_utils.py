45607ea1ebc43a7395b40d714abce77e14ad40a5
popiol <opiola.piotr@gmail.com>
2020-08-06 12:51:27 +0200

    price up/down

--- docker/ml/app/ml_utils.py
+++ docker/ml/app/ml_utils.py
@@ -333,6 +333,7 @@ def compare_agents(agent1, agent2, hist=False, quick=False):
         scores2.append(agent2.score)
         print("Capital:", agent1.get_capital(), agent2.get_capital())
         print("Bought/Sold:", agent1.n_bought, "/", agent1.n_sold, "-", agent2.n_bought, "/", agent2.n_sold)
+        print("Price up/down:", np.average(agent1.price_up), "/", np.average(agent1.price_down))
     score1 = np.average(scores1) + min(scores1) / 3
     score2 = np.average(scores2) + min(scores2) / 3
     return score1, score2
